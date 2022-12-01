c3101e2837ecdb7f146f987f6ef3d00b809c6846
choihs0457 <85881597+choihs0457@users.noreply.github.com>
2022-12-01 09:32:00 +0900

    22.12.01 end

--- src/driver.c
+++ src/driver.c
@@ -1,5 +1,20 @@
 #include "rbtree.h"
+#include <stdio.h>
+#include <stdlib.h>
+// int main(int argc, char *argv[])
+// {
+//     printf("hihi\n");
+//     return 0;
+// }
 
-int main(int argc, char *argv[]) {
+int main()
+{
+    rbtree *t = new_rbtree();
+    rbtree_insert(t, 10);
+    rbtree_insert(t, 5);
+    rbtree_insert(t, 20);
 
+    printf("root: %d, left: %d, right: %d\n", t->root->left->color, t->root->right->color);
+
+    return 0;
 }
